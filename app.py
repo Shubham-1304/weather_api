5e5f67bb00a324e15fe447b86164321e3c439222
shubh123 <shubh@example.com>
2021-09-08 21:52:15 +0530

    app

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from weathers import Tempratures,Weather,AllWeather
+
+app=Flask(__name__)
+
+api=Api(app)
+
+
+
+api.add_resource(Weather,'/weather/<string:city>')
+api.add_resource(AllWeather,'/AllWeather')
+api.add_resource(Tempratures,'/temperatures/<string:city>')
+
+if __name__=='__main__':
+    app.run(port=5000,debug=True)
