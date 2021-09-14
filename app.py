826abbb256202bd1f6f2de8ac7110cebbecc80a4
shubh123 <shubh@example.com>
2021-09-14 18:29:29 +0530

    port added

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from weathers import Tempratures,Weather,AllWeather
+import os
 
 app=Flask(__name__)
 
@@ -14,4 +15,5 @@ api.add_resource(AllWeather,'/AllWeather')
 api.add_resource(Tempratures,'/temperatures/<string:city>')
 
 if __name__=='__main__':
-    app.run(port=5000,debug=True)
+    app.debug=True
+    app.run(host="0.0.0.0",port=int(os.environ.get('PORT',5000)))
